dc2d2c0e18a42cacd9a7f268edc280d189a38dd1
Orguntay <67823328+Orguntay@users.noreply.github.com>
2021-01-18 01:41:11 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void GetList()
+        {
+            Console.WriteLine("Müşteriler listelendi");
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri güncellendi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
